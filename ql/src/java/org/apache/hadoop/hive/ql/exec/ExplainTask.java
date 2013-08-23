de5ac35ff3fa149a622dd925102c2ce047484d9b
code6 <wzc1989@gmail.com>
2013-08-23 16:37:27 +0800

    Revert " HIVE-4397 Show stage dependency as a tree"
    This reverts commit 985b83c86eebcfdc2949bc9b98948dab7df55c54.

--- ql/src/java/org/apache/hadoop/hive/ql/exec/ExplainTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/ExplainTask.java
@@ -142,17 +142,6 @@ public class ExplainTask extends Task<ExplainWork> implements Serializable {
       outJSONObject.put("STAGE DEPENDENCIES", jsonDependencies);
     }
 
-    String tree = Task.toString(work.getRootTasks());
-    if (out != null) {
-      out.println("STAGE GRAPH:\n" + tree);
-      out.println();
-    }
-
-    if (jsonOutput) {
-      outJSONObject.put("STAGE GRAPH", tree);
-    }
-
-
     // Go over all the tasks and dump out the plans
     JSONObject jsonPlan = outputStagePlans(out, work, work.getRootTasks(), 0);
 
